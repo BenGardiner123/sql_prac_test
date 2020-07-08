ee33c5a7c471138a2a20190b38dd3d6d29700d7f
Ben Gardiner <bennygards@gmail.com>
2020-07-08 10:52:55 +1000

    Task 2 complete

--- prac_test3.sql
+++ prac_test3.sql
@@ -1,6 +1,6 @@
 -- 102151272 BEN GARDINER
 
--- tASK 2 SCHEMA
+-- tASK 1 SCHEMA
 
 /* 
 SUBJECT (SubjCode, Description)
@@ -24,3 +24,47 @@ FK:SubjCode REFERENCES SUBJECT
 FK:StaffID REFERENCES TEACHER 
 
 */
+
+-- Task 2
+
+CREATE TABLE SUBJECT (
+    SubjCode NVARCHAR(100),
+    Description NVARCHAR(500)
+)
+
+CREATE TABLE STUDENT (
+    StudentID NVARCHAR(10), 
+    Surname NVARCHAR(100) NOT NULL, 
+    GivenName NVARCHAR(100) NOT NULL, 
+    Gender NVARCHAR(1) CONSTRAINT Check_student_gender CHECK (Gender = "M""F""I") NULL,
+    PRIMARY KEY (StudentID)
+);
+
+CREATE TABLE TEACHER (
+    StaffID INT CONSTRAINT Check_teacher_staff_id CHECK (LENGTH(StaffID) = 8), 
+    Surname NVARCHAR(100) NOT NULL, 
+    GivenName NVARCHAR(100) NOT NULL
+);
+
+CREATE TABLE ENROLLMENT (
+    StudentID NVARCHAR(10), 
+    Year INT CONSTRAINT Check_Enrollment_Year CHECK (LENGTH (Year) = 4), 
+    SubjCode NVARCHAR(100) , 
+    Semester INT CONSTRAINT Check_Enrollment_Semester CHECK (Semester = 1 OR Semester = 2), 
+    DateEnrolled DATE, 
+    Grade NVARCHAR(2) CONSTRAINT Check_Enrollment_Grade CHECK (Grade = 'N''P''C''D''HD') NULL,
+    PRIMARY KEY (StudentID, Year, Semester, StaffID, SubjCode),
+    FOREIGN KEY (StudentID) REFERENCES STUDENT,
+    FOREIGN KEY (SubjCode, Year, Semester) REFERENCES SUBJECT_OFFERING
+);
+
+CREATE TABLE SUBJECT_OFFERING (
+    SubjCode NVARCHAR(100), 
+    StaffID INT CONSTRAINT Check_SubjectOffering_staff_id CHECK (LENGTH(StaffID) = 8) NULL, 
+    Year INT CONSTRAINT Check_SubjectOffering_Year CHECK (LENGTH (Year) = 4), 
+    Semester INT CONSTRAINT Check_SubjectOffering_Semester CHECK (Semester = 1 OR Semester = 2),  
+    Fee MONEY CONSTRAINT Check_SubjectOffering_Fee CHECK (Fee > 0)
+);
+
+SELECT * 
+FROM sys.tables;
