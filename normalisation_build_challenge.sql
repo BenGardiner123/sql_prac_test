3a7239ed61e508146f6c09b5d9b2ebbb9edd73dc
Ben Gardiner <bennygards@gmail.com>
2020-07-16 09:41:08 +1000

    adding some data to test and play around with

--- normalisation_build_challenge.sql
+++ normalisation_build_challenge.sql
@@ -7,11 +7,11 @@
 -- DROP TABLE CUSTOMER;
 -- DROP TABLE BOOKING; 
 
-SELECT 
+/*  SELECT 
     *
 FROM
-    information_schema.tables;
-
+    information_schema.tables; */
+/*
 
 CREATE TABLE STAFF (
 StaffLastname NVARCHAR(100) NOT NULL,	  
@@ -61,14 +61,49 @@ PRIMARY KEY (CustomerID, WarehouseLocation, CourseID, CourseDate),
 FOREIGN KEY (WarehouseLocation, CourseID, CourseDate) REFERENCES COURSE_OFFERING,
 FOREIGN KEY (CustomerID) REFERENCES CUSTOMER
 );
-
-CREATE TABLE STAFF_ALLOCATION (
-StaffID	NVARCHAR(100) NOT NULL,
+*/
+/* CREATE TABLE STAFF_ALLOCATION (
+StaffID NVARCHAR(100) NOT NULL,
 CourseDate DATE CHECK (LEN(CourseDate) = 8),
 WarehouseLocation NVARCHAR(100) NOT NULL,
 CourseID NVARCHAR(25) NOT NULL,
 PRIMARY KEY (StaffID, CourseDate, CourseID, WarehouseLocation),
 FOREIGN KEY (StaffID) REFERENCES STAFF,
-FOREIGN KEY (CourseDate, WarehouseLocation, CourseID) REFERENCES COURSE_OFFERING
-);
- 
+FOREIGN KEY (WarehouseLocation, CourseID, CourseDate) REFERENCES COURSE_OFFERING,
+);  */
+
+INSERT INTO STAFF (StaffID, StaffFirstName, StaffLastname) VALUES
+('10000212',	'Ben',	'Gardiner'),
+('10000213',    'Harry',	'McLarry'),
+('10000214',	'Toni',     'Tony'); 
+
+INSERT INTO WAREHOUSE (WarehouseLocation) VALUES
+('OAKLIEGH'),
+('SKYE'), 
+('DANDENONG');
+
+INSERT INTO CUSTOMER (CustomerID, CustomerName, CustomerAddress, CustomerCity) VALUES
+('193840923847', 'Barry', 'Welcome Street', 'Melbourne'),
+('193834923847', 'Sally', 'Mrfode Street', 'Melbourne'),
+('134403563847', 'Larry', 'Harfaor Street', 'Melbourne');
+
+INSERT INTO COURSE (CourseID, CourseName) VALUES
+('BRICK_101', 'Basic Bricklaying'),
+('CARPENTRY_102', 'Basic Carpentry'),
+('PLUMBING_103', 'How to fix your toilet');
+
+INSERT INTO COURSE_OFFERING (WarehouseLocation, CourseID, CourseDate) VALUES
+('OAKLIEGH','BRICK_101','2020-06-15'),
+('SKYE','CARPENTRY_102', '2020-06-15'), 
+('DANDENONG','PLUMBING_103', '2020-06-15');
+
+INSERT INTO BOOKING (CustomerID, WarehouseLocation, CourseID, CourseDate, BookingDate, PaymentAmount, Review) VALUES
+('193840923847', 'Oakleigh', 'BRICK_101', '2020-06-15', '2020-03-15', '500.00', NULL),
+('193834923847', 'Skye', 'CARPENTRY_102', '2020-06-15', '2020-03-32', '400.00', NULL),
+('134403563847', 'Dandenong', 'PLUMBING_103', '2020-06-15', '2020-03-23', '600.00', 'This course was amazing i especiialy like the instructor Toni');
+
+INSERT INTO STAFF_ALLOCATION (StaffID, CourseDate, WarehouseLocation, CourseID) VALUES
+('10000212','2020-06-15','OAKLIEGH', 'BRICK_101'),
+('10000213', '2020-06-15', 'SKYE', 'CARPENTRY_102'),
+('10000214','2020-06-15','DANDENONG','PLUMBING_103');  
+
