d1bf104e08e46471ff733de6ab9ddd0c388d48df
Ben Gardiner <bennygards@gmail.com>
2020-07-16 09:48:12 +1000

    made mistake using check len 8 for date..

--- normalisation_build_challenge.sql
+++ normalisation_build_challenge.sql
@@ -11,7 +11,7 @@
     *
 FROM
     information_schema.tables; */
-/*
+
 
 CREATE TABLE STAFF (
 StaffLastname NVARCHAR(100) NOT NULL,	  
@@ -42,7 +42,7 @@ PRIMARY KEY (CourseID)
 CREATE TABLE COURSE_OFFERING (
 WarehouseLocation NVARCHAR(100) NOT NULL,
 CourseID NVARCHAR(25) NOT NULL,
-CourseDate DATE CHECK (LEN(CourseDate) = 8),
+CourseDate DATE,
 PRIMARY KEY (WarehouseLocation,CourseID,CourseDate), 
 FOREIGN KEY (CourseID) REFERENCES COURSE,
 FOREIGN KEY (WarehouseLocation) REFERENCES WAREHOUSE
@@ -52,8 +52,8 @@ CREATE TABLE  BOOKING (
 CustomerID NVARCHAR(50) NOT NULL,	
 WarehouseLocation NVARCHAR(100) NOT NULL,	
 CourseID NVARCHAR(25) NOT NULL,	
-CourseDate DATE CHECK (LEN(CourseDate) = 8),
-BookingDate	DATE CHECK (LEN(BookingDate) = 8),
+CourseDate DATE,
+BookingDate	DATE,
 PaymentAmount MONEY CHECK (PaymentAmount > 0) NOT NULL, 
 Review NVARCHAR(100) NULL,
 
@@ -61,7 +61,8 @@ PRIMARY KEY (CustomerID, WarehouseLocation, CourseID, CourseDate),
 FOREIGN KEY (WarehouseLocation, CourseID, CourseDate) REFERENCES COURSE_OFFERING,
 FOREIGN KEY (CustomerID) REFERENCES CUSTOMER
 );
-*/
+
+
 /* CREATE TABLE STAFF_ALLOCATION (
 StaffID NVARCHAR(100) NOT NULL,
 CourseDate DATE CHECK (LEN(CourseDate) = 8),
@@ -70,9 +71,9 @@ CourseID NVARCHAR(25) NOT NULL,
 PRIMARY KEY (StaffID, CourseDate, CourseID, WarehouseLocation),
 FOREIGN KEY (StaffID) REFERENCES STAFF,
 FOREIGN KEY (WarehouseLocation, CourseID, CourseDate) REFERENCES COURSE_OFFERING,
-);  */
+);  
 
-INSERT INTO STAFF (StaffID, StaffFirstName, StaffLastname) VALUES
+/* INSERT INTO STAFF (StaffID, StaffFirstName, StaffLastname) VALUES
 ('10000212',	'Ben',	'Gardiner'),
 ('10000213',    'Harry',	'McLarry'),
 ('10000214',	'Toni',     'Tony'); 
@@ -88,22 +89,22 @@ INSERT INTO CUSTOMER (CustomerID, CustomerName, CustomerAddress, CustomerCity) V
 ('134403563847', 'Larry', 'Harfaor Street', 'Melbourne');
 
 INSERT INTO COURSE (CourseID, CourseName) VALUES
-('BRICK_101', 'Basic Bricklaying'),
-('CARPENTRY_102', 'Basic Carpentry'),
-('PLUMBING_103', 'How to fix your toilet');
+('BRICK101', 'Basic Bricklaying'),
+('CARPENTRY102', 'Basic Carpentry'),
+('PLUMBING103', 'How to fix your toilet');
 
 INSERT INTO COURSE_OFFERING (WarehouseLocation, CourseID, CourseDate) VALUES
-('OAKLIEGH','BRICK_101','2020-06-15'),
-('SKYE','CARPENTRY_102', '2020-06-15'), 
-('DANDENONG','PLUMBING_103', '2020-06-15');
+('OAKLIEGH','BRICK101','2020-06-15'),
+('SKYE','CARPENTRY102', '2020-06-15'), 
+('DANDENONG','PLUMBING103', '2020-06-15');
 
 INSERT INTO BOOKING (CustomerID, WarehouseLocation, CourseID, CourseDate, BookingDate, PaymentAmount, Review) VALUES
-('193840923847', 'Oakleigh', 'BRICK_101', '2020-06-15', '2020-03-15', '500.00', NULL),
-('193834923847', 'Skye', 'CARPENTRY_102', '2020-06-15', '2020-03-32', '400.00', NULL),
-('134403563847', 'Dandenong', 'PLUMBING_103', '2020-06-15', '2020-03-23', '600.00', 'This course was amazing i especiialy like the instructor Toni');
+('193840923847', 'Oakleigh', 'BRICK101', '2020-06-15', '2020-03-15', '500.00', NULL),
+('193834923847', 'Skye', 'CARPENTRY102', '2020-06-15', '2020-03-32', '400.00', NULL),
+('134403563847', 'Dandenong', 'PLUMBING103', '2020-06-15', '2020-03-23', '600.00', 'This course was amazing i especiialy like the instructor Toni');
 
 INSERT INTO STAFF_ALLOCATION (StaffID, CourseDate, WarehouseLocation, CourseID) VALUES
-('10000212','2020-06-15','OAKLIEGH', 'BRICK_101'),
-('10000213', '2020-06-15', 'SKYE', 'CARPENTRY_102'),
-('10000214','2020-06-15','DANDENONG','PLUMBING_103');  
+('10000212','2020-06-15','OAKLIEGH', 'BRICK101'),
+('10000213', '2020-06-15', 'SKYE', 'CARPENTRY102'),
+('10000214','2020-06-15','DANDENONG','PLUMBING103');   */
 
