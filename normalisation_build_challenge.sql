69d05a3793079e059680304ee44b3f703b4cd2f5
Ben Gardiner <bennygards@gmail.com>
2020-07-16 10:06:30 +1000

    more issues- case sensitive?

--- normalisation_build_challenge.sql
+++ normalisation_build_challenge.sql
@@ -1,20 +1,20 @@
 
+DROP TABLE STAFF;  
+DROP TABLE COURSE;
+DROP TABLE WAREHOUSE;
+DROP TABLE CUSTOMER;
+DROP TABLE BOOKING;
+DROP TABLE COURSE_OFFERING;  
+DROP TABLE STAFF_ALLOCATION;
 
 
--- DROP TABLE STAFF;  
--- DROP TABLE COURSE;
--- DROP TABLE WAREHOUSE;
--- DROP TABLE CUSTOMER;
--- DROP TABLE BOOKING;
--- DROP TABLE COURSE_OFFERING;  
--- DROP TABLE STAFF_ALLOCATION;
 
-/* SELECT *
+SELECT *
 FROM
-information_schema.tables;  */
+information_schema.tables; 
 
 
-CREATE TABLE STAFF (
+/* CREATE TABLE STAFF (
 StaffLastname NVARCHAR(100) NOT NULL,	  
 StaffFirstName	NVARCHAR(100) NOT NULL,	
 StaffID	NVARCHAR(100) NOT NULL,
@@ -78,7 +78,7 @@ SELECT
 FROM
     information_schema.tables;  
 
-/* INSERT INTO STAFF (StaffID, StaffFirstName, StaffLastname) VALUES
+INSERT INTO STAFF (StaffID, StaffFirstName, StaffLastname) VALUES
 ('10000212',	'Ben',	'Gardiner'),
 ('10000213',    'Harry',	'McLarry'),
 ('10000214',	'Toni',     'Tony'); 
@@ -104,14 +104,14 @@ INSERT INTO COURSE_OFFERING (WarehouseLocation, CourseID, CourseDate) VALUES
 ('DANDENONG','PLUMBING103', '2020-06-15');
 
 INSERT INTO BOOKING (CustomerID, WarehouseLocation, CourseID, CourseDate, BookingDate, PaymentAmount, Review) VALUES
-('193840923847', 'Oakleigh', 'BRICK101', '2020-06-15', '2020-03-15', '500.00', NULL),
-('193834923847', 'Skye', 'CARPENTRY102', '2020-06-15', '2020-03-32', '400.00', NULL),
-('134403563847', 'Dandenong', 'PLUMBING103', '2020-06-15', '2020-03-23', '600.00', 'This course was amazing i especiialy like the instructor Toni');
+('193840923847', 'OAKLIEGH', 'BRICK101', '2020-06-15', '2020-03-15', '500.00', NULL),
+('193834923847', 'SKYE', 'CARPENTRY102', '2020-06-15', '2020-03-32', '400.00', NULL),
+('134403563847', 'DANDENONG', 'PLUMBING103', '2020-06-15', '2020-03-23', '600.00', 'This course was amazing i especiialy like the instructor Toni');
 
 INSERT INTO STAFF_ALLOCATION (StaffID, CourseDate, WarehouseLocation, CourseID) VALUES
 ('10000212','2020-06-15','OAKLIEGH', 'BRICK101'),
 ('10000213', '2020-06-15', 'SKYE', 'CARPENTRY102'),
 ('10000214','2020-06-15','DANDENONG','PLUMBING103');  
 
-GO */
+GO 
 
